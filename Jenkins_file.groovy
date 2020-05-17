3a1538ca75edea0c1828d5e3f34102d4cb16c54b
joele <joele8323@gmail.com>
2020-05-17 20:44:55 +0300

    Create Jenkins_file.groovy

---
+++ Jenkins_file.groovy
@@ -0,0 +1,10 @@
+properties([parameters([string(defaultValue: '24', description: '', name: 'NAME', trim: false)]), pipelineTriggers([pollSCM('* * * * *')])])
+
+node{
+    stage("clone"){
+        git "https://github.com/YoelEigner/Class_Files.git"
+    }
+    stage("show"){
+        bat "dir"
+    }
+}
